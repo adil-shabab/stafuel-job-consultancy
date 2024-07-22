b9a4a8088f7f11b2bfed2663ddecbdff0423d3db
adil-shabab <adilshabab2004@gmail.com>
2024-07-22 17:32:20 +0530

    contact form created

--- website/urls.py
+++ website/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path('about', about, name='about'), 
     path('jobs', jobs, name='jobs'), 
     path('blogs', blogs, name='blogs'), 
-    path('blogs/title', single_blog, name='single_blog'), 
+    path('blogs/a-comprehensive-guide-to-job-consultancy', single_blog_1, name='single_blog_1'), 
     path('contact', contact, name='contact'), 
     path('upload/resume', upload_resume, name='upload_resume'), 
     path('jobs/<slug:slug>', application, name='application'), 
     path('terms', terms, name='terms'), 
+    path('filter_jobs/', filter_jobs, name='filter_jobs'),
+
 
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import *
+from django.db.models import Q
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.contrib import messages as msg
+
 
 # Create your views here.
 def home(request):
@@ -12,37 +17,101 @@ def home(request):
     return render(request, 'frontend/home.html', context)
 
 
-def about(request):
-    context = {
-
-    }
-    return render(request, 'frontend/about.html', context)
-
 
 def jobs(request):
-    context = {
+    jobs = Job.objects.all()
+    departments = Department.objects.all()
 
+    query = request.GET.get('q')
+    location = request.GET.get('location')
+    department_id = request.GET.get('department')
+
+    if query:
+        jobs = jobs.filter(title__icontains=query)
+    if location:
+        jobs = jobs.filter(location__icontains=location)
+    if department_id:
+        jobs = jobs.filter(department_id=department_id)
+
+    context = {
+        'jobs': jobs,
+        'departments': departments,
+        'query': query,
+        'location': location,
+        'department_id': department_id,
     }
     return render(request, 'frontend/jobs.html', context)
 
+
+
+
+def filter_jobs(request):
+    jobs = Job.objects.all()
+    
+    job_type = request.GET.get('job_type')
+    experience = request.GET.get('experience')
+    q = request.GET.get('q')
+    location = request.GET.get('location')
+    department = request.GET.get('department')
+
+    if job_type:
+        jobs = jobs.filter(job_type=job_type)
+    if experience:
+        jobs = jobs.filter(experience=experience)
+    if q:
+        jobs = jobs.filter(title__icontains=q)
+    if location:
+        jobs = jobs.filter(location__icontains=location)
+    if department:
+        jobs = jobs.filter(department_id=department)
+    
+    job_count = jobs.count()
+    html = render_to_string('frontend/include/job_list.html', {'jobs': jobs})
+    
+    return JsonResponse({'html': html, 'count': job_count})
+
+
+
+
+
+
+def about(request):
+    return render(request, 'frontend/about.html')
+
+
 def blogs(request):
-    context = {
+    return render(request, 'frontend/blogs.html')
 
-    }
-    return render(request, 'frontend/blogs.html', context)
-def single_blog(request):
-    context = {
 
-    }
-    return render(request, 'frontend/blog-detail.html', context)
+def single_blog_1(request):
+    return render(request, 'frontend/blog-detail-1.html')
+
 
 
 def contact(request):
-    context = {
-
-    }
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message_text = request.POST['message']
+        mobile = request.POST['mobile']
+        
+        # Create a new Message object and save it
+        message = Message(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message_text,
+            number=mobile
+        )
+        message.save()
+        msg.success(request, "Message Sent Successfully")
+        
+        # Redirect to a success page or show a success message
+        return redirect('contact')
+    
+    context = {}
     return render(request, 'frontend/contact.html', context)
-
 
 def upload_resume(request):
     context = {
