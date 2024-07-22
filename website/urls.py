873d310b5252ab81ee7b9d4039d7cb8eef023523
adil-shabab <adilshabab2004@gmail.com>
2024-07-22 15:57:09 +0530

    Terms and condition page created

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('jobs', jobs, name='jobs'), 
     path('blogs', blogs, name='blogs'), 
     path('blogs/title', single_blog, name='single_blog'), 
+    path('contact', contact, name='contact'), 
+    path('upload/resume', upload_resume, name='upload_resume'), 
+    path('jobs/<slug:slug>', application, name='application'), 
+    path('terms', terms, name='terms'), 
 
 ]
--- website/views.py
+++ website/views.py
@@ -35,3 +35,30 @@ def single_blog(request):
 
     }
     return render(request, 'frontend/blog-detail.html', context)
+
+
+def contact(request):
+    context = {
+
+    }
+    return render(request, 'frontend/contact.html', context)
+
+
+def upload_resume(request):
+    context = {
+
+    }
+    return render(request, 'frontend/resume.html', context)
+
+
+def application(request, slug):
+    job = Job.objects.get(slug=slug)
+
+    context = {
+        'job': job
+    }
+    return render(request, 'frontend/application.html', context)
+
+
+def terms(request):
+    return render(request, 'frontend/terms.html')
